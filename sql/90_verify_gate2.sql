043271a319ccfc104911a2fade6eb3dbce861633
Owen Guobadia <owenguobadia24s@gmail.com>
2026-01-16 22:19:10 +0000

    Fix table name references in SQL scripts for consistency

--- sql/90_verify_gate2.sql
+++ sql/90_verify_gate2.sql
@@ -1,5 +1,5 @@
 -- 1) Tables exist
-select to_regclass('ovc.ovc_blocks_v01.1_min') as min_table;
+select to_regclass('ovc.ovc_blocks_v01_1_min') as min_table;
 select to_regclass('ovc.ovc_run_reports_v01') as run_reports_table;
 
 -- 2) Views compile
@@ -12,9 +12,9 @@ select count(*) from ovc.v_data_quality_ohlc_basic;
 
 -- 3) State key sanity (non-empty)
 select count(*) as missing_state_key
-from ovc.ovc_blocks_v01.1_min
+from ovc.ovc_blocks_v01_1_min
 where state_key is null or length(state_key) = 0;
 
 -- 4) Enum sanity
-select bias_dir, count(*) from ovc.ovc_blocks_v01.1_min group by bias_dir;
-select pred_dir, count(*) from ovc.ovc_blocks_v01.1_min group by pred_dir;
+select bias_dir, count(*) from ovc.ovc_blocks_v01_1_min group by bias_dir;
+select pred_dir, count(*) from ovc.ovc_blocks_v01_1_min group by pred_dir;
